6d3391d13b9a6daf6695bb9d722e0f162696e9e3
graphicmismatch <riptide.syncopate@gmail.com>
2024-06-05 15:55:24 +0530

    implemented slowtext update

--- SovietGameJamm/Assets/Scripts/Utility/Dialog/DialogHelper.cs
+++ SovietGameJamm/Assets/Scripts/Utility/Dialog/DialogHelper.cs
@@ -39,16 +39,8 @@ public class DialogHelper : MonoBehaviour
         ResponsePanelHelper.clearResponses();
         if (inst.di) {
             inst.ind = 0;
-            UIManager.updateTextDisplay(inst.di.lines[0]);
-            if (inst.di.lines.Length > 1)
-            {
-                ResponsePanelHelper.addResponse(inst.di.defResponses[0]);
-            }
-            else {
-                foreach (Options op in inst.di.opt) {
-                    ResponsePanelHelper.addResponse(op.optName,op.next);
-                }
-            }
+            UIManager.updateTextDisplay(inst.di.lines[0],inst.di,0.1f);
+            
         }
     }
 
@@ -63,18 +55,8 @@ public class DialogHelper : MonoBehaviour
         ResponsePanelHelper.clearResponses();
         if (inst.ind <= inst.di.lines.Length - 1)
         {
-            UIManager.updateTextDisplay(inst.di.lines[inst.ind]);
-            if (inst.ind <= inst.di.defResponses.Length - 1)
-            {
-                ResponsePanelHelper.addResponse(inst.di.defResponses[inst.ind]);
-            }
-            else
-            {
-                foreach (Options op in inst.di.opt)
-                {
-                    ResponsePanelHelper.addResponse(op.optName, op.next);
-                }
-            }
+            UIManager.updateTextDisplay(inst.ind,inst.di.lines[inst.ind], inst.di, 0.1f);
+
         }
         else {
             UIManager.toggleSpeakingPanel(false);
