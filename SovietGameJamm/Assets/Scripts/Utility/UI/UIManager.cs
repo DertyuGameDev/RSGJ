b234f3860c0eda4149b331bced805b39050a98ff
Dertyu <110996909+DertyuGameDev@users.noreply.github.com>
2024-06-05 12:47:13 +0200

    2DPolygon

--- SovietGameJamm/Assets/Scripts/Utility/UI/UIManager.cs
+++ SovietGameJamm/Assets/Scripts/Utility/UI/UIManager.cs
@@ -8,7 +8,6 @@ public class UIManager : MonoBehaviour
     public static UIManager inst;
     public GameObject speakingPanel;
     public TMP_Text dlog;
-    public TMP_Text nam;
     // Start is called before the first frame update
     void Awake()
     {
@@ -24,63 +23,8 @@ public class UIManager : MonoBehaviour
     public static void toggleSpeakingPanel(bool s) {
         inst.speakingPanel.SetActive(s);
     }
-    public static void updateTextDisplay(string s,Dialogue d, float t)
+    public static void updateTextDisplay(string s)
     {
-        inst.StartCoroutine(inst.slowType(s, d, t));
-    }
-    public IEnumerator slowType(string s, Dialogue di, float tBWc) {
-        dlog.text = "";
-        foreach (char c in s) {
-            dlog.text += c.ToString();
-            yield return new WaitForSeconds(tBWc);
-        }
-        if (di.lines.Length > 1)
-        {
-            ResponsePanelHelper.addResponse(di.defResponses[0]);
-        }
-        else
-        {
-            foreach (Options op in di.opt)
-            {
-                ResponsePanelHelper.addResponse(op.optName, op.next);
-            }
-        }
-    }
-
-    public static void updateTextDisplay(int ind,string s, Dialogue d, float t)
-    {
-
-        inst.StartCoroutine(inst.slowType(ind,s, d, t));
-    }
-    public IEnumerator slowType(int ind, string s, Dialogue di, float tBWc)
-    {
-        dlog.text = "";
-        foreach (char c in s)
-        {
-            dlog.text += c.ToString();
-            yield return new WaitForSeconds(tBWc);
-        }
-        if (ind <= di.defResponses.Length - 1)
-        {
-            ResponsePanelHelper.addResponse(di.defResponses[ind]);
-        }
-        else
-        {
-            foreach (Options op in di.opt)
-            {
-                ResponsePanelHelper.addResponse(op.optName, op.next);
-            }
-        }
-    }
-    public static void updateNameDisplay(string s,bool isLeftTalking)
-    {
-        inst.nam.text = s;
-        if (isLeftTalking)
-        {
-            inst.nam.horizontalAlignment = HorizontalAlignmentOptions.Left;
-        }
-        else {
-            inst.nam.horizontalAlignment = HorizontalAlignmentOptions.Right;
-        }
+        inst.dlog.text = s;
     }
 }
